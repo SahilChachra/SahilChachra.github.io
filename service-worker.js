1121192e3e12c4294f3a61b98c0e87a6382763d2
Sahil Chachra <37156032+SahilChachra@users.noreply.github.com>
2022-01-22 15:12:28 +0530

    Updated portfolio

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e39fbb46ef17703d7f71536b25e5214.js"
+  "/precache-manifest.6a55ee099eca7dfdd2971d201eeebf11.js"
 );
 
 self.addEventListener('message', (event) => {
